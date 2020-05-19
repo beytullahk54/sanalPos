dc361531ecaea1ea1dc961fbfafa7dc82d5c3ccb
beytullah <32388523+beytullahk54@users.noreply.github.com>
2020-05-19 04:01:35 +0300

    test

--- resources/views/home/collection.blade.php
+++ resources/views/home/collection.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php 
-    $SiteAyarlar = DB::table( 'site_ayarlar' )
+    \App\SiteAyarlar = DB::table( 'site_ayarlar' )
                      ->where('Id','1')
                      ->get();
     
@@ -26,11 +26,11 @@
 
     </style>
 
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ URL::asset($SiteAyarlar[0]->SiteFavicon)}}?v2">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ URL::asset(\App\SiteAyarlar[0]->SiteFavicon)}}?v2">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.5.3/css/bootstrapValidator.css"/>
-    <title>{{$SiteAyarlar[0]->SiteAdi}} - {{$education->education_name}}</title>
+    <title>{{\App\SiteAyarlar[0]->SiteAdi}} - {{$education->education_name}}</title>
 
   </head>
   <body>
@@ -39,7 +39,7 @@
         <!-- Image and text -->
         <nav class="navbar navbar-light bg-light">
             <a class="navbar-brand" >
-            <img src="https://lsmdpusem.dpu.edu.tr/plugins/images/dpusem.png"  style="width:2%"alt=""> {{$SiteAyarlar[0]->SiteAdi}}
+            <img src="https://lsmdpusem.dpu.edu.tr/plugins/images/dpusem.png"  style="width:2%"alt=""> {{\App\SiteAyarlar[0]->SiteAdi}}
             </a>
         </nav>
         <nav aria-label="breadcrumb">
@@ -77,7 +77,7 @@
                     <input type="hidden" name="lang" value="tr">
                     <input type="hidden" name="price1" value="{{ $odeme['amount']}}">
                     <input type="hidden" name="total1" value="{{ $odeme['amount']}}">
-                    <input type="hidden" name="firmaadi" value="{{$SiteAyarlar[0]->SiteAdi}}">
+                    <input type="hidden" name="firmaadi" value="{{\App\SiteAyarlar[0]->SiteAdi}}">
 
 
                     <input type="hidden" name="Fismi" :value="fIsmi" >
